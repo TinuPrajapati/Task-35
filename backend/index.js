61b06077ce2f1c06d02ccc4a71aa1c36e81667f7
TinuPrajapati <tinup2580@gmail.com>
2024-09-22 14:49:32 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(
 // Database connection
 async function main() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    await mongoose.connect(process.env.mongo_Altas);
     console.log("Database connected");
   } catch (error) {
     console.log("Database connection failed", error);
